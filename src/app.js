0b5d07a222dc5c2b9d600c630e8c97d476972f82
syibran malawi <branmalawi@gmail.com>
2023-07-12 13:40:10 +0700

    setup project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+// melakukan request http get
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.get("/*", (req, res) => {
+  res.sendStatus(404);
+});
+
+// gunakan method listen untuk menjalankan server
+const port = 6060;
+app.listen(port, () => {
+  console.log("server berjalan pada port : " + port);
+});
